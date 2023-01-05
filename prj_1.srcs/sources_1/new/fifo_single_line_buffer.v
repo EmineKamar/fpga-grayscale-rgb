fc97a2329044a936185e14ce10b0fdbee42b13bc
EmineKamar <74881273+EmineKamar@users.noreply.github.com>
2023-01-05 23:25:52 +0300

    Add files via upload

---
+++ prj_1.srcs/sources_1/new/fifo_single_line_buffer.v
@@ -0,0 +1,93 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.01.2023 19:18:23
+// Design Name: 
+// Module Name: fifo_single_line_buffer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fifo_single_line_buffer(
+    input clk,
+    input rst,
+    input we_i,
+    input [7:0] data_i,
+    output [7:0] data_o,
+    output done_o
+    );
+
+    //parameter DEPTH = 640;
+    parameter DEPTH = 5;
+    reg [7:0] mem [0 : DEPTH -1] ;
+
+    reg [9:0] wr_pointer ; //640 -->
+    reg [9:0] rd_pointer ;
+
+     reg [9:0] iCounter ;
+
+     assign done_o = (iCounter == DEPTH)? 1:0;
+     assign data_o = mem[rd_pointer];
+
+     //handle with iCounter
+     always @(posedge clk) begin
+        if (rst) begin
+            iCounter <=0;
+        end else begin
+            if (we_i== 1) begin
+                iCounter <= (iCounter == DEPTH)? iCounter : iCounter + 1;
+            end
+            
+        end
+        
+     end
+
+     //handle with write process
+
+     always @(posedge clk) begin
+        if (rst) begin
+            wr_pointer <=0;
+        end else begin
+            if (we_i== 1) begin
+                mem[wr_pointer]<= data_i;
+                wr_pointer <= (wr_pointer == DEPTH-1)? 0 : wr_pointer + 1;
+            end
+            
+        end
+        
+     end
+
+     //handle with read process
+
+     always @(posedge clk) begin
+        if (rst) begin
+            rd_pointer <=0;
+        end else begin
+            if (we_i== 1) begin
+                if (iCounter == DEPTH) begin
+                    rd_pointer <= (rd_pointer == DEPTH-1)? 0 : rd_pointer + 1;
+                end
+            end
+            
+        end
+        
+     end
+
+     
+
+
+
+
+endmodule
---
+++ prj_1.srcs/sources_1/new/rgb_to_grayscale.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.01.2023 18:57:42
+// Design Name: 
+// Module Name: rgb_to_grayscale
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rgb_to_grayscale(
+    input clk,
+    input rst,
+
+    input [7:0] red_i,
+    input [7:0] green_i,
+    input [7:0] blue_i,
+
+    input done_i,
+
+    output reg [7:0] grayscale_o,
+    output reg done_o
+
+    );
+
+    always @(posedge clk) begin
+        if (rst) begin
+            grayscale_o <= 0;
+            done_o <= 0;
+        end else begin
+            if (done_i == 1) begin
+                grayscale_o <= (red_i >>2)  +  (red_i>>5) +
+                               (green_i >>1) + (green_i>>4)+
+                               (blue_i>>4)   + (blue_i>>5);
+                done_o <= 1'b1;
+            end else begin
+                grayscale_o <= 0;
+                done_o <= 0;
+            end
+        end
+    end
+endmodule
